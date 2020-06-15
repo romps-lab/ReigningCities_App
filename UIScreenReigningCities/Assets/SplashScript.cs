98ea0f8e06ba5bd7f63451c0a253cfc3cac91ac9
Lakshmi Ramesh <42285183+lakshmi-ramesh@users.noreply.github.com>
2020-06-15 23:15:50 +1000

    Splash Screen Code
    Splash Screen Code
    Splash screen image
    Scene
    And C# script for fade in and out

---
+++ UIScreenReigningCities/Assets/SplashScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScript : MonoBehaviour
+{
+    private bool finished = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DoFade());
+    }
+
+    IEnumerator DoFade()
+    {
+        CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+        while(canvasGroup.alpha<1)
+        {
+            canvasGroup.alpha += Time.deltaTime;
+            yield return null;
+
+        }
+        yield return new WaitForSeconds(6);
+        //To fade Out
+        /*while(canvasGroup.alpha>0)
+        {
+            canvasGroup.alpha -= Time.deltaTime;
+            yield return null;
+        }*/
+
+        yield return finished = true;
+    }
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        if(finished)
+        {
+            SceneManager.LoadScene("Main", LoadSceneMode.Single);
+        }
+    }
+}
